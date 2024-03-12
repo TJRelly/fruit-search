96bf3d6001a1a41aacd9f9f6312bdd2da2083bab
tjrelly <tjrelly@89527327-152196735-2857928193.89527327-152196735-2857928193>
2024-03-11 20:38:59 -0400

    completed fruit-search

---
+++ script.js
@@ -0,0 +1,116 @@
+const input = document.querySelector("#fruit");
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+function search(inputStr) {
+  suggestions.innerText = "";
+  suggestions.classList.add("has-suggestions");
+
+  if (inputStr.length) {
+    const filteredFruit = fruit.filter((fruitItem) =>
+      fruitItem.toLocaleLowerCase().includes(inputStr.toLowerCase())
+    );
+    showSuggestions(filteredFruit);
+  }
+}
+
+function searchHandler() {
+  search(input.value);
+}
+
+function showSuggestions(fruitItems) {
+  fruitItems.slice(0, 5).map((fruitItem) => {
+    const fruitSuggestion = document.createElement("li");
+    fruitSuggestion.innerText = fruitItem;
+    suggestions.append(fruitSuggestion);
+  });
+}
+
+function useSuggestion(e) {
+  input.value = e.target.innerText;
+  suggestions.innerText = "";
+}
+
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
